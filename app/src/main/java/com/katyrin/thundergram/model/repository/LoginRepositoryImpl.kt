c56e1ef7ea0fbb1773e718c44a0e710b8a245daf
romankatyrin <rkatyrin@gmail.com>
2023-08-20 23:04:55 +0600

    Updated libraries (v1.1.4)

--- app/src/main/java/com/katyrin/thundergram/model/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/katyrin/thundergram/model/repository/LoginRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.katyrin.thundergram.model.repository
 
+import android.content.Context
+import android.widget.Toast
 import com.katyrin.libtd_ktx.core.TelegramFlow
 import com.katyrin.libtd_ktx.coroutines.*
 import com.katyrin.libtd_ktx.flows.authorizationStateFlow
@@ -7,7 +9,9 @@ import com.katyrin.thundergram.model.mapping.LoginMapping
 import com.katyrin.thundergram.model.storage.Storage
 import com.katyrin.thundergram.viewmodel.appstates.AuthState
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.withContext
 import org.drinkless.td.libcore.telegram.TdApi
 import javax.inject.Inject
 
@@ -16,6 +20,7 @@ class LoginRepositoryImpl @Inject constructor(
     private val loginMapping: LoginMapping,
     private val storage: Storage,
     private val dispatcher: CoroutineDispatcher,
+    private val context: Context,
     override val api: TelegramFlow
 ) : LoginRepository {
 
@@ -36,6 +41,9 @@ class LoginRepositoryImpl @Inject constructor(
             .flowOn(dispatcher)
 
     private suspend fun checkRequiredParams(state: TdApi.AuthorizationState?) {
+        withContext(Dispatchers.Main) {
+            Toast.makeText(context, state.toString(), Toast.LENGTH_SHORT).show()
+        }
         when (state) {
             is TdApi.AuthorizationStateWaitTdlibParameters -> api.setTdlibParameters(parameters)
             is TdApi.AuthorizationStateWaitEncryptionKey -> api.checkDatabaseEncryptionKey()
